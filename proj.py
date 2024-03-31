d6236efdc0f524e64ac32bceb07d9d586535f061
LucaM185 <lucamig2004@gmail.com>
2024-03-31 22:03:36 +0200

    Stripped code to a mimum that works

--- proj.py
+++ proj.py
@@ -1,57 +1,43 @@
+import matplotlib.pyplot as plt
 from picograd import * 
 import pickle
 
+# Dataset loading 
 with open('./mnist.pickle', 'rb') as f:
     dataset = pickle.load(f)
 
-# dataset = [(dataset[i]).data for i in range(dataset.shape[0]) if dataset[i, 0].data.item() in [0, 1]]
-# dataset = Tensor(np.array(dataset))
+# Dataset preprocessing
+X_train, y_train = dataset[:, 1:]/255, dataset[:, 0]
+print(X_train.sum(-1))
 
-print(dataset.shape)
-
+# Model definition
+batch_size = 128
 classes = 10
 fc0 = Linear(784, classes)
+optim = SGD([fc0.weight, fc0.bias], lr=0.01)
 
-in_features, out_features = 784, classes
-weight = Randn((in_features, out_features), requires_grad=True) 
-bias = Randn((1, out_features), requires_grad=True) # double check the 1 in (1, out_features)
-
-
-X_train, y_train = dataset[:, 1:]/255, dataset[:, 0]
-
-print(X_train.sum(-1))
-batch_size = 128
-
+# Training loop
 lossi = []
 acci = []
-optim = SGD([fc0.weight, fc0.bias], lr=0.03)
 for i in range(0, 800):
     idx = np.random.randint(0, dataset.shape[0], (batch_size,))
     labels = y_train[idx].onehot(classes)
 
-    pro = fc0(X_train[idx])# @ weight + bias
-    # pro = X_train[idx] @ weight + bias
-    out = (pro).softmax()  # (5, 10) + (1, 10)
-    diff = out - labels # (5, 10) 
-    diffs = (diff)**2
-    loss = (diffs).sum()
+    n1 = fc0(X_train[idx])
+    loss = ((n1.softmax() - labels)**2).sum()
     loss.numpy()
     loss.backward()
     optim.step()
 
     if i % 100 == 0: 
         print("Loss: ", round(loss.numpy()/batch_size, 2))
-        print("Accuracy: ", round(((out.argmax(-1).data == y_train[idx].data)).mean(), 2))
+        print("Accuracy: ", round(((n1.argmax(-1).data == y_train[idx].data)).mean(), 2))
         print()
 
     loss.zero_grad()
 
-
     lossi.append(loss.numpy())
-    acci.append(((out.argmax(-1).data == y_train[idx].data)).mean())
-
-
-import matplotlib.pyplot as plt
+    acci.append(((n1.argmax(-1).data == y_train[idx].data)).mean())
 
 # DRAW LOSS and ACCURACY in the same plot
 plt.plot(np.array(lossi)/max(lossi), label='loss')
